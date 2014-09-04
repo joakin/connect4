87ee8475332172ca66f3a962bc0b6053602eeea3
Joakin <joaquin@chimeces.com>
2014-09-04 21:43:22 +0200

    Move test terminal stuff, clean index

---
+++ src/terminal.js
@@ -0,0 +1,39 @@
+
+console.log('Hi! Welcome to connect4');
+
+var Connect4 = require('./game');
+
+console.log('Init game');
+var game = Connect4.init();
+
+Connect4.print(game);
+
+var started = Connect4.start('John', 'Mary', game);
+
+console.log(
+  'Game started with',
+  started.players.blue,
+  'as BLUE and',
+  started.players.red,
+  'as RED'
+);
+
+Connect4.print(started);
+
+console.log('Blue puts');
+
+var p = Connect4.play(3, started);
+Connect4.print(p);
+
+p = Connect4.play(3, p);
+Connect4.print(p);
+
+[0, 1, 2].forEach(function(m) {
+  p = Connect4.play(m, p);
+  if (m !== 2)
+    p = Connect4.play(m, p);
+});
+
+Connect4.print(p);
+
+
